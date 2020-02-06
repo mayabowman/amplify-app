e76b52587eafa3816079ff1f4074a86de371b706
Maya Bowman <mayashady1@gmail.com>
2020-02-06 14:02:08 -0500

    removed bullets, console.logs

--- src/Solutions/Solutions.js
+++ src/Solutions/Solutions.js
@@ -3,10 +3,8 @@ import './Solutions.css'
 
 class Solutions extends React.Component {
   render() {
-    console.log('props', this.props)
     let dropDownClass;
     this.props.show ? dropDownClass = 'drop-down open' : dropDownClass = 'drop-down'
-    console.log('class', dropDownClass)
     return (
       <ul className={dropDownClass}>
         <li>Solutions By Industry</li>
