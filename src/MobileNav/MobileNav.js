0c0a812556307e3b91e6ac614b60ad807f3890a1
Maya Bowman <mayashady1@gmail.com>
2020-02-06 18:51:48 -0500

    added mobile styling

--- src/MobileNav/MobileNav.js
+++ src/MobileNav/MobileNav.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './MobileNav.css';
 
 class MobileNav extends React.Component {
   render() {
     return (
       <div>
-        <div className='moble-nav'>
-          <Link to={'/mobilesolutions'}>
+        <div className='mobile-nav__link'>
+          <Link
+            className='mobile-nav'
+            to={'/mobilesolutions'}
+          >
             SOLUTIONS
           </Link>
         </div>
-        <div>
+        <div className='mobile-nav__link'>
           PRODUCTS
         </div>
-        <div>
+        <div className='mobile-nav__link'>
           AUTOMATION
         </div>
-        <div>
+        <div className='mobile-nav__link'>
           EDUCATION
         </div>
-        <div>
+        <div className='mobile-nav__link'>
           RESOURCES
         </div>
       </div>
--- src/MobileSolutions/MobileSolutions.js
+++ src/MobileSolutions/MobileSolutions.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './MobileSolutions.css';
 
 class MobileSolutions extends React.Component {
   render() {
     return (
       <div>
-        <div>
-          <Link to={'/mobilenav'}>
+        <div className='mobile-solutions__link'>
+          <Link
+            className='mobile-nav-link'
+            to={'/mobilenav'}
+          >
             GO BACK
           </Link>
         </div>
-        <div>
+        <div className='mobile-solutions__link'>
           SOLUTIONS BY INDUSTRY
         </div>
-        <div>
+        <div className='mobile-solutions__link'>
           SOLUTIONS BY PROCESS
         </div>
-        <div>
+        <div className='mobile-solutions__link'>
           SOFTWARE SOLUTIONS
         </div>
-        <div>
+        <div className='mobile-solutions__link'>
           SERVICES
         </div>
       </div>
