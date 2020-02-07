98eee6f24b657e962291c9c199f88ba81e936168
Maya Bowman <mayashady1@gmail.com>
2020-02-06 21:56:51 -0500

    Updates

---
+++ precache-manifest.2fd2371820aa95b652bcad4d53370ab3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c73a665c20f5070f433de26d6414d76f",
+    "url": "/FED-exercise/index.html"
+  },
+  {
+    "revision": "3d29dd9af2d123315ef6",
+    "url": "/FED-exercise/static/css/main.567bf09e.chunk.css"
+  },
+  {
+    "revision": "97c402aa347a415f1a1f",
+    "url": "/FED-exercise/static/js/2.5890e641.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/FED-exercise/static/js/2.5890e641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d29dd9af2d123315ef6",
+    "url": "/FED-exercise/static/js/main.77f417e3.chunk.js"
+  },
+  {
+    "revision": "d5c231e7eb0749848676",
+    "url": "/FED-exercise/static/js/runtime-main.3af455f0.js"
+  },
+  {
+    "revision": "4af4ca46db9b4da6442f986113234916",
+    "url": "/FED-exercise/static/media/logo-large.4af4ca46.png"
+  }
+]);
